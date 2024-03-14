a8fb37642759f1394faebd80e3d126c579755e89
gzavlanis <gzavlanis@gmail.com>
2024-03-14 18:08:10 +0200

    add authenticators

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
 import Afroditi from './calendars/afroditi'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Chris from './calendars/chris'
 import Home from './calendars/home'
 import Login from './authenticator/login'
-import { AuthMiddleware } from './authenticator/authMiddleware'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(() => localStorage.getItem('logged_user') !== null)
+
+  useEffect(() => {
+    localStorage.setItem('logged_user', JSON.stringify(isLoggedIn))
+  }, [isLoggedIn])
+
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route
-          index
-          path="/home"
-          element={
-            <AuthMiddleware>
-              <Home />
-            </AuthMiddleware>
-          }
-        />
-        <Route
-          path="/afroditi"
-          element={
-            <AuthMiddleware>
-              <Afroditi />
-            </AuthMiddleware>
-          }
-        />
-        <Route
-          path="/chris"
-          element={
-            <AuthMiddleware>
-              <Chris />
-            </AuthMiddleware>
-          }
-        />
+        <Route index path="/home" element={isLoggedIn ? <Home /> : <Navigate to='/' />} />
+        <Route path="/afroditi" element={isLoggedIn ? <Afroditi /> : <Navigate to='/' />} />
+        <Route path="/chris" element={isLoggedIn ? <Chris /> : <Navigate to='/' />} />
         <Route path="*" element={<Login />} />
       </Routes>
     </div>
